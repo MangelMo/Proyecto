861d0f14512e0ff27800f210fe36cf8a96f809a7
MangelMo <149389953+MangelMo@users.noreply.github.com>
2024-05-21 18:12:26 -0500

    Proyecto

---
+++ src/main/java/com/AngelMozombite/proyect/Clase1.java
@@ -0,0 +1,16 @@
+package com.AngelMozombite.proyect;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Clase1 {
+    String nombre;
+    double altura;
+    String color;
+    int edad;
+
+}
